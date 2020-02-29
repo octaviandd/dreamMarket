e1f8d58257c2ccb9a849a992adddc9d0ed6cfa8b
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-02-29 19:52:50 +0000

    Update actionType.js

--- client/src/store/actions/actionType.js
+++ client/src/store/actions/actionType.js
@@ -1,5 +1,9 @@
 /** @format */
 
 export const ADD_DREAM = "ADD_DREAM";
+export const GET_DREAMS = "GET_DREAMS";
 export const ADD_STORY = "ADD_STORY";
 export const ADD_SYMBOL = "ADD_SYMBOL";
+export const GET_DREAM = "GET_DREAM";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const GET_COMMENTS = "GET_COMMENTS";
