680f52fe015ea455347570aaa8be07ce06d5e9d0
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-02-29 19:49:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 const db = config.get("mongoURI");
 
 //ROUTES
+const dreams = require("./routes/api/dreamsRoute");
+const comments = require("./routes/api/commentsRoute");
 
 //CONNECT TO MONGODB
 mongoose
@@ -26,6 +28,9 @@ mongoose
   .then(() => console.log("MONGODB CONNECTED"))
   .catch(err => console.log(err));
 
+app.use("/api/dreams", dreams);
+app.use("/api/comments", comments);
+
 //SERVER STATIC ASSTES IF IN PRODUCTION
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -36,6 +41,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //CREATE PORT 3000
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("PORT IS UP AND RUNNING AT" + port));
