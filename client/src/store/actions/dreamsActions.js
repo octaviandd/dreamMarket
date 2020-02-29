05d146c17045a09356278a1af452bb1ba978a6a7
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-02-29 19:54:32 +0000

    Update dreamsActions.js

--- client/src/store/actions/dreamsActions.js
+++ client/src/store/actions/dreamsActions.js
@@ -1,8 +1,27 @@
 /** @format */
 
-import { ADD_DREAM } from "./actionType";
+import { ADD_DREAM, GET_DREAMS, GET_DREAM } from "./actionType";
+import axios from "axios";
 
-export const addDream = item => ({
-  type: ADD_DREAM,
-  payload: item
+export const addDream = item => dispatch => {
+  axios.post("/api/dreams", item).then(res =>
+    dispatch({
+      type: ADD_DREAM,
+      payload: res.data
+    })
+  );
+};
+
+export const getDreams = () => dispatch => {
+  axios.get("/api/dreams").then(res => {
+    dispatch({
+      type: GET_DREAMS,
+      payload: res.data
+    });
+  });
+};
+
+export const getDream = id => dispatch => ({
+  type: GET_DREAM,
+  id
 });
