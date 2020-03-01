29d859c22ab3555c1fad8c7a5530c8b2c5fe7d2d
Octavian David <45077244+octaviandd@users.noreply.github.com>
2020-03-01 23:22:53 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@
 
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import ArrivePage from "./pages/ArrivePage";
 import DreamsPage from "./pages/DreamsPage";
 import StoriesPage from "./pages/StoriesPage";
 import SymbolsPage from "./pages/SymbolsPage";
+import DreamPage from "./pages/DreamPage";
+import ErrorPage from "./pages/ErrorPage.js";
 import store from "./store";
 import { Provider } from "react-redux";
+import SymbolPage from "./pages/SymbolPage";
 
 function App() {
-  console.log("hello");
   return (
     <Provider store={store}>
       <NavBar />
@@ -21,6 +25,9 @@ function App() {
         <Route exact path="/dreams" component={DreamsPage}></Route>
         <Route exact path="/stories" component={StoriesPage}></Route>
         <Route exact path="/symbols" component={SymbolsPage}></Route>
+        <Route exact path="/symbols/:slug" component={SymbolPage}></Route>
+        <Route exact path="/dreams/:slug" component={DreamPage}></Route>
+        <Route exact path="/error" component={ErrorPage}></Route>
       </Switch>
     </Provider>
   );
